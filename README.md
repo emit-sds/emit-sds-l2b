fa94c52439a29c079cccf17c37ef1b15e0ac9a68
pgbrodrick <phil.brodrick@gmail.com>
2020-11-25 14:49:28 -0800

    update tetracorder run scripts and the README

--- README.md
+++ README.md
@@ -7,16 +7,11 @@ The repository supports two methods for creating mineral maps.  The first method
 1) Generating mineral maps via tetracorder
 2) Post-processing these maps to the EMIT-10 estimated spectral abundance
 
-Step 1) can be executed with:
+These steps can be run in a coordinated fashion using:
 
-> sh run_tetracorder.sh [l2a_reflectance_file] [scaled_reflectance_destination] [tetracorder_output_directory]
+> sh run_tetracorder.sh [tetracorder_output_directory] [aggregated_output_base] [l2a_reflectance_file] [l2a_reflectance_uncertainty_file] 
 
-where the latter two arguments are output locations.
-
-Before postprocessing tetracorder output, compile the program "specpr2envi" and use it to convert the tetracorder "specpr" format library into an equivalent ENVI spectral library.  Extra header fields contain critical ancillary information like the record numbers and names. Syntax:
- 
+Before running, compile the program "specpr2envi" and use it to convert the tetracorder "specpr" format library into an equivalent ENVI spectral library.  Extra header fields contain critical ancillary information like the record numbers and names.  This step must be repeated for each reference library used (multiple are supported). Syntax:
 > specpr2envi [specpr_library_path] [output_envi_library_path] 
 
-A header file will be created automatically. Then run the aggregation script.  It requires a translation file in comma-separated value format.  The translation file holds record numbers and spectrum names together with a 10-dimensional vector of mineral fraction coefficients for each spectrum.  These coefficients indicate how to portion the baand depth estimate into EMIT mineral classes.  An example of this format is: "convert_tetracorder/mineral_fractions_s06an14a.csv".  The aggregator syntax is: 
 
-> python3 aggregator.py [tetra_expert_system_path] [tetra_enviFormat_library_path] [tetra_output_base_directory] [translation_file] [output_l2b]
--- run_tetracorder.sh
+++ run_tetracorder.sh
@@ -1,48 +1,32 @@
  
-REFL_FILE=${1}
-SCALED_REFL=${2}
-SCALED_REFL_TETRA_REF=${3}
-TETRA_OUT_DIR=${4}
-AGGREGATED_OUT_FILE=${5}
+TETRA_OUT_DIR=${1}
+AGGREGATED_OUT_FILE=${2}
+REFL_FILE=${3}
+REFL_UNCERT_FILE=${4}
 
 echo 'starting'
 date
 
 TETRA_OUT_DIR_ABS=`readlink -f ${TETRA_OUT_DIR}`
+TETRA_SETUP=/shared/tetracorder5.26a.cmds/cmd-setup-tetrun
 
-TETRA_EXE=/t1/tetracorder.cmds/tetracorder5.2a.cmds/cmd-setup-tetrun
-TETRA_EXPERT_SYSTEM=cmd.lib.setup.t5.2a2
-
-#AVCL
-#TETRA_ENVI_SL=/beegfs/scratch/brodrick/emit/emit-sds-l2b/convert_tetracorder/Spectral-Library-Reader-master/output_spectral_library_avcl
-#ANG
-TETRA_ENVI_SL=/beegfs/scratch/brodrick/emit/emit-sds-l2b/convert_tetracorder/Spectral-Library-Reader-master/output_spectral_library
-
-#/t1 - Tetracorder directories and commands
-echo 'scaling'
-date
-export GDAL_CACHEMAX=1
-python scale_and_cast_refl.py ${REFL_FILE} ${SCALED_REFL}
+REFL_ABS_FILE=`readlink -f ${REFL_FILE}`
 
 echo 'tetracorder'
 date
 #AVCL
-#${TETRA_EXE} ${TETRA_OUT_DIR} aviris_2013 cube ${SCALED_REFL_TETRA_REF} -20 80 C .5 1.5 bar
+#${TETRA_SETUP} ${TETRA_OUT_DIR} aviris_2018 cube ${REFL_FILE} -20 80 C .5 1.5 bar
 #ANG
-${TETRA_EXE} ${TETRA_OUT_DIR} avirisng_2014b cube ${SCALED_REFL_TETRA_REF} -20 80 C .5 1.5 bar
-echo 20 > ${TETRA_OUT_DIR}/TETNCPU.txt; 
+${TETRA_SETUP} ${TETRA_OUT_DIR} avirisng_2014b cube ${REFL_FILE} 1 -T -20 80 C -P .5 1.5 bar
 
 current_dir=${PWD}
 cd ${TETRA_OUT_DIR}
-time ./cmd.runtet cube >& cmd.runtet.out 
+time ./cmd.runtet cube ${REFL_ABS_FILE} >& cmd.runtet.out
 cd ${current_dir}
  
 
 echo 'aggregation'
 date
-python /beegfs/scratch/brodrick/emit/emit-sds-l2b/convert_tetracorder/aggregator.py ${TETRA_EXPERT_SYSTEM} ${TETRA_ENVI_SL} ${TETRA_OUT_DIR_ABS} /beegfs/scratch/brodrick/emit/emit-sds-l2b/convert_tetracorder/mineral_fractions_s06an14a.csv ${AGGREGATED_OUT_FILE} -calculate_uncertainty 1 -reflectance_file ${REFL_FILE} -reflectance_uncertainty_file ${6}
-#TETRA_EXPERT_SYSTEM, TETRA_LIBRARY_FILE, TETRA_OUTPUT_DIR, MINERAL_FRACTIONS_FILE, OUTPUT
+python aggregator.py ${TETRA_OUT_DIR_ABS}  ${AGGREGATED_OUT_FILE} -calculate_uncertainty 1 -reflectance_file ${REFL_FILE} -reflectance_uncertainty_file ${REFL_UNCERT_FILE}
 
 
-# Spectral library build command
-#./specpr2envi /sl1/usgs/library06.conv/s06av13a output_spectral_library_avcl
